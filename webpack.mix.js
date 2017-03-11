7a94022d9c77d62eb881031abf7cc2029b2d2675
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2017-03-12 03:38:07 +0700

    Update the explanation

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,7 @@ require('./webpack.config')
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
+ | for your application.
  |
  */
 
