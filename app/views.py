aac75dbe63e24692973536aafa3fe5634952ec70
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2017-03-12 03:36:28 +0700

    Add missing future unicode literals

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView as View
